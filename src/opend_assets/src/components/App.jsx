3b3012a9dd1bfec30d3f9cf98288ebad57678a56
zane falcao <falcaozane@gmail.com>
2024-07-01 23:06:33 +0530

    Initial

---
+++ src/opend_assets/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+
+  //const NFTID="rrkah-fqaaa-aaaaa-aaaaq-cai"
+
+  return (
+    <div className="App">
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/opend_assets/src/components/Item.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/logo.png";
+import { HttpAgent, Actor} from "@dfinity/agent"
+import { idlFactory } from "../../../declarations/nft";
+import { Principal } from "@dfinity/principal"
+import Button from "./Button";
+
+function Item(props) {
+
+  const [name, setName] = useState();
+  const [owner, setOwner] = useState();
+  const [image, setImage] = useState();
+  const [button, setButton] = useState()
+  const [priceInput, setPriceInput] = useState()
+
+  const id = props.id;
+
+  const localHost = "http://localhost:8080/";
+
+  const agent = new HttpAgent({host: localHost});
+
+  async function loadNFT(){
+    const NFTActor = await Actor.createActor(idlFactory,{
+      agent,
+      canisterId:id,
+    })
+
+    const name = await NFTActor.getName();
+    const owner = await NFTActor.getOwner();
+    const imageData = await NFTActor.getAsset();
+    const imageContent = new Uint8Array(imageData);
+    const image = URL.createObjectURL(new Blob([imageContent.buffer], {type:"image/png"}))
+    setName(name);
+    setOwner(owner.toText());
+    setImage(image);
+    setButton(<Button handleClick={handleSell} title={"Sell"} />)
+  }
+
+  useEffect(() => {
+    loadNFT();
+  }, []);
+
+  let price;
+  function handleSell() {
+    setPriceInput(
+      <input
+        placeholder="Price in DANG"
+        type="number"
+        className="price-input"
+        value={price}
+        onChange={(e)=> (price=e.target.value)}
+      />
+    );
+    setButton(<Button handleClick={handleSell} text={"Confirm"} />)
+  }
+  
+
+  return (
+    <div className="disGrid-item">
+      <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
+        <img
+          className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
+          src={image}
+        />
+        <div className="disCardContent-root">
+          <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
+            <span className="purple-text">{name}</span>
+          </h2>
+          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
+            Owner: {owner}
+          </p>
+          {priceInput}
+          {button}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
